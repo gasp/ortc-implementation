06e9f628a898127d46269291f494be8936ff8770
gaspard <gaspard@gmail.com>
2015-11-27 21:08:24 +0100

    exchanging peers availability

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,1 +1,5 @@
 var socket = io();
+
+socket.on('peers', function (peers) {
+  console.log(peers);
+});
--- sockets/main.js
+++ sockets/main.js
@@ -1,5 +1,24 @@
 module.exports = function (io) {
+  var peers = [];
   io.on('connection', function (socket) {
+    var peer = {
+      id: socket.id,
+      address: socket.handshake.address,
+      ua: socket.handshake.headers['user-agent']
+    };
+    peers.push(peer);
+    io.sockets.emit('peers', peers);
+
+    socket.on('disconnect', function() {
+      for (var i = 0; i < peers.length; i++) {
+        if(socket.id === peers[i].id) {
+          peers.splice(i, 1);
+          break;
+        }
+      }
+      io.sockets.emit('peers', peers);
+    });
+
     console.log('connected');
   });
 };
