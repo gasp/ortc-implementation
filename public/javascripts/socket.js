05b54554ebd09deecaa52f383b3ff21ada3a832e
gaspard <gaspard@gmail.com>
2015-11-27 21:20:00 +0100

    peers: displaying availability

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,5 +1,18 @@
 var socket = io();
 
 socket.on('peers', function (peers) {
-  console.log(peers);
+  var $peers = document.querySelector("#peers");
+  while ($peers.firstChild) {
+    $peers.removeChild($peers.firstChild);
+  }
+  for (var i = 0; i < peers.length; i++) {
+    var peer = document.createElement('li');
+    var ua = 'Unknown';
+    if (/Chrome/i.test(peers[i].ua)) ua = 'Chrome';
+    if (/Firefox/i.test(peers[i].ua)) ua = 'Firefox';
+    if (/Edge/i.test(peers[i].ua)) ua = 'Edge';
+    peer.innerHTML = ua + ' ' + peers[i].address;
+    $peers.appendChild(peer);
+  }
+  console.log('peers', peers);
 });
