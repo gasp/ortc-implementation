71fea502c15b6da398197819facc8543ff08bc1e
gaspard <gaspard@gmail.com>
2015-11-29 10:35:39 +0100

    adding log

---
+++ public/javascripts/log.js
@@ -0,0 +1,10 @@
+var log = function () {
+  if (typeof jQuery !== 'undefined' && $('#log').length) {
+    for (var i = 0; i < arguments.length; i++) {
+      var div = document.createElement('div');
+      $(div).html(arguments[i]);
+      $('#log').append(div);
+    }
+  }
+  console.log(arguments);
+};
